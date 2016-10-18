3e56adb5d46295a7a8137b6c9bb0b6b1089f0e87
Chaeyun Yang <ycy6685@naver.com>
2016-10-18 19:42:40 +0900

    #4 Button Event Programming

--- app/src/main/java/kr/hs/emirim/dbfl0406/simplediary/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbfl0406/simplediary/MainActivity.java
@@ -1,12 +1,16 @@
 package kr.hs.emirim.dbfl0406.simplediary;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
 
@@ -37,6 +41,21 @@ public class MainActivity extends AppCompatActivity {
                 String content=readDiary(fileName);
                 editDiary.setText(content);
                 butSave.setEnabled(true);
+            }
+        });
+
+        butSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    FileOutputStream out=openFileOutput(fileName, Context.MODE_WORLD_WRITEABLE);
+                    String diaryContents=editDiary.toString();
+                    out.write(diaryContents.getBytes());
+                    out.close();
+                    Toast.makeText(getApplicationContext(),"저장이 완료되었습니다",Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
